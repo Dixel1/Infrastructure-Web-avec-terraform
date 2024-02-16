e2a30f11747005792caeed36a081e7205d173c42
Dixel <63664894+Dixel1@users.noreply.github.com>
2024-02-16 17:42:59 +0100

    Update launch_template.tf

--- launch_template.tf
+++ launch_template.tf
@@ -1,16 +1,16 @@
-#ressource "aws_launch_template" "launchTemplate-schaffnerkelian" {
- # name = "launchTemplate-schaffnerkelian"
-  #image_id = "ami-0c55b159cbfafe1f0"
-#  instance_type = "t2.micro"
- # key_name = "launchTemplate-schaffnerkelian"
-  #network_interfaces {
-#    device_index = 0
- #   associate_public_ip_address = true
-  #}
-#  security_group_names = ["sg-04891020a5d7968f5"]
- # user_data = <<-EOF
-  #              #!/bin/bash
-   #             cd /home/ubuntu/app/
-    #            sudo docker compose up --build -d
-     #           EOF
-#}
+resource "aws_launch_template" "launchtemplate-schaffnerkelian" {
+  name = "launchtemplate-schaffnerkelian"
+  image_id = "ami-012ba92271e91512d"
+  instance_type = "t2.micro"
+  key_name = "launchtemplate-schaffnerkelian"
+  network_interfaces {
+    device_index = 0
+    associate_public_ip_address = true
+  }
+  security_group_names = ["sg-04891020a5d7968f5"]
+  user_data = <<-EOF
+                #!/bin/bash
+                cd /home/ubuntu/app/
+                sudo docker compose up --build -d
+                EOF
+}
